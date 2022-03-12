c593f164ad7653c3bceb2c45bc4382e2ea9afe9b
Prajwal2345 <prajwal06314902020@msijanakpuri.com>
2022-03-12 20:00:06 +0530

    third commit

---
+++ Prime.java
@@ -0,0 +1,18 @@
+class Prime {
+    public static void main(String args[]) {
+        int a = Integer.parseInt(args[0]);
+        boolean flag=false;
+        for (int i = 2; i <= a/2; ++i) {
+            if(a % i == 0)
+            {
+                flag=true;
+                break;
+            }
+        }
+        if(!flag)
+            System.out.println(a + " is prime number.");
+
+        else    
+            System.out.println(a + " is not a prime number.");
+    }
+}
