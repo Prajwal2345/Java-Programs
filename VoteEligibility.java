164e7b3e7144a5eaee5000fd5c7f866ed9530532
Prajwal2345 <prajwal06314902020@msijanakpuri.com>
2022-03-12 19:59:40 +0530

    second commit

---
+++ VoteEligibility.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class vote
+{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Age: ");
+        int age = sc.nextInt();
+    }
+}
